0472b485e1bd228b4b5e57d8be8c4f35110ae4de
guigo <[GuilhermeGomes@fam.edu.br]>
2025-05-22 20:23:22 -0300

    Adicionar arquivos de projeto.

---
+++ Equipamento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Equipamento
+    {
+        public int Armor { get; set; }
+        public string WeaponName { get; set; }
+        public int WeaponDamage { get; set; } 
+        public int Shield { get; set; }
+
+
+        public static int StocDamage(bool Acertou, Chara atacou)
+        {
+
+            return Dice.rng.Next(1, 9) + atacou.STR;
+        }
+
+        public static int GSdamage(bool acertou,  Chara atacou) { return Dice.rng.Next(2, 13) + atacou.STR; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp2;
+
+namespace Game
+{
+    
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string EscolhaArma;
+            
+            Chara person = new Chara(3, 1, 2);
+            
+
+            Console.WriteLine("Vc pega a espadona ou espadinha?");
+            EscolhaArma = Console.ReadLine().ToLower();
+            int DanoCausado = 0;
+
+            if (EscolhaArma == "espadona")
+            {
+                Console.WriteLine("Brabo, vc escolheu a " + EscolhaArma);
+                EscolhaArma = "teste";
+                DanoCausado = person.Ataque(EscolhaArma, true);
+            } else if (EscolhaArma == "espadinha")
+            {
+                Console.WriteLine("Brabo, vc escolheu a " + EscolhaArma);
+                EscolhaArma = "teste2";
+                DanoCausado = person.Ataque(EscolhaArma, true);
+            }
+            
+
+            Console.WriteLine("O dano foi " + DanoCausado);
+
+
+                Console.ReadKey();
+
+        }
+    }
+}
